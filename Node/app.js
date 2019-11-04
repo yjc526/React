1a1013c60969cae8ed84446aba4e7834343cc769
yjc526 <yjc526@naver.com>
2019-11-04 16:34:48 +0900

    basic commit

---
+++ Node/app.js
@@ -0,0 +1,7 @@
+const logger = require("./logger");
+
+const wrapper = () => {
+    logger("프로그램 실행중..."); 
+};
+
+module.exports = wrapper;
---
+++ Node/logger.js
@@ -0,0 +1,8 @@
+const url = "https://naver.com";
+
+console.log("111111111111111111");
+const log = (msg) => {
+    console.log(`logging Message : ${msg}`);
+}
+
+module.exports = log;
