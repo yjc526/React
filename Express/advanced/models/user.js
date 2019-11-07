3538f6aae0a785de3548bb0d061f0644cf95395c
yjc526 <yjc526@naver.com>
2019-11-07 18:09:16 +0900

    session

--- Express/advanced/models/user.js
+++ Express/advanced/models/user.js
@@ -3,26 +3,25 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-    name: { type: String, required: true, minlength: 3, maxlength: 30 },
-    article: [{ type: mongoose.Types.ObjectId, ref: "Article" }]
+  name: { type: String, required: true, minlength: 3, maxlength: 30 },
+  articles: [{ type: mongoose.Types.ObjectId, ref: "Article" }]
 });
 
 const User = model("User", userSchema);
 
 function validateUser(user) {
-    const schema = Joi.object({
-        name: Joi.string()
-            .alphanum()
-            .min(3)
-            .max(3)
-            .require(),
-        article: Joi.array().items(Joi.string())
-    });
+  const schema = Joi.object({
+    name: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required(),
+    articles: Joi.array().items(Joi.string())
+  });
 
-    return schema.validate(user);
+  return schema.validate(user);
 }
-
 module.exports = {
-    User,
-    validateUser
-}
+  User,
+  validateUser
+};
